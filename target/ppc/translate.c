3a223ab2081c7a358631848563ea5a3d5e679225
Rot127 <unisono@quyllur.org>
2022-06-17 15:11:05 -0400

    Trace `ori`

--- target/ppc/translate.c
+++ target/ppc/translate.c
@@ -2727,10 +2727,12 @@ static void gen_ori(DisasContext *ctx)
 {
     target_ulong uimm = UIMM(ctx->opcode);
 
+    log_load_gpr(rS(ctx->opcode));
     if (rS(ctx->opcode) == rA(ctx->opcode) && uimm == 0) {
         return;
     }
     tcg_gen_ori_tl(cpu_gpr[rA(ctx->opcode)], cpu_gpr[rS(ctx->opcode)], uimm);
+    log_store_gpr(rA(ctx->opcode));
 }
 
 /* oris */
