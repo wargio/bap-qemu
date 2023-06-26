9018d02d7cb0e6142d8b91b3156f1f3792831315
wargio <wargio@libero.it>
2023-06-26 22:48:30 +0800

    Fixed trace

--- target/sh4/trace_helper.c
+++ target/sh4/trace_helper.c
@@ -22,7 +22,6 @@ void HELPER(trace_endframe)(CPUSH4State *env, target_ulong old_pc)
 	qemu_trace_endframe(env, old_pc, 32);
 }
 
-/*
 OperandInfo * load_store_reg(uint32_t reg, uint32_t val, int ls)
 {
     RegOperand * ro = g_new(RegOperand,1);
@@ -53,7 +52,7 @@ OperandInfo * load_store_reg(uint32_t reg, uint32_t val, int ls)
 
 void HELPER(trace_load_reg)(uint32_t reg, uint32_t val)
 {
-    qemu_log("This register (r%d) was read. Value 0x%x\n", reg, val);
+    // qemu_log("This register (r%d) was read. Value 0x%x\n", reg, val);
 
     //r0 always reads 0
     OperandInfo *oi = load_store_reg(reg, (reg != 0) ? val : 0, 0);
@@ -63,58 +62,59 @@ void HELPER(trace_load_reg)(uint32_t reg, uint32_t val)
 
 void HELPER(trace_store_reg)(uint32_t reg, uint32_t val)
 {
-    qemu_log("This register (r%d) was written. Value: 0x%x\n", reg, val);
+    // qemu_log("This register (r%d) was written. Value: 0x%x\n", reg, val);
 
     OperandInfo *oi = load_store_reg(reg, val, 1);
 
     qemu_trace_add_operand(oi, 0x2);
 }
 
-// TODO: signature has changed, see arm
-OperandInfo * load_store_mem(uint32_t addr, uint32_t val, int ls, int len) {
-    MemOperand * mo = g_new(MemOperand,1);
+OperandInfo *load_store_mem(uint64_t addr, int ls, const void *data, size_t data_size) {
+    MemOperand * mo = g_new(MemOperand, 1);
     mem_operand__init(mo);
 
     mo->address = addr;
 
-    OperandInfoSpecific *ois = g_new(OperandInfoSpecific,1);
+    OperandInfoSpecific *ois = g_new(OperandInfoSpecific, 1);
     operand_info_specific__init(ois);
     ois->mem_operand = mo;
 
-    OperandUsage *ou = g_new(OperandUsage,1);
+    OperandUsage *ou = g_new(OperandUsage, 1);
     operand_usage__init(ou);
     if (ls == 0) {
         ou->read = 1;
     } else {
         ou->written = 1;
     }
-    OperandInfo *oi = g_new(OperandInfo,1);
+    OperandInfo *oi = g_new(OperandInfo, 1);
     operand_info__init(oi);
-    oi->bit_length = len*8;
+    oi->bit_length = data_size * 8;
     oi->operand_info_specific = ois;
     oi->operand_usage = ou;
-    oi->value.len = len;
+    oi->value.len = data_size;
     oi->value.data = g_malloc(oi->value.len);
-    memcpy(oi->value.data, &val, len);
-
+    #ifdef BSWAP_NEEDED
+    memcpy_rev(oi->value.data, data, data_size);
+    #else
+    memcpy(oi->value.data, data, data_size);
+    #endif
     return oi;
 }
 
-void HELPER(trace_ld)(CPUMIPSState *env, uint32_t val, uint32_t addr)
+void HELPER(trace_ld)(CPUSH4State *env, uint32_t val, uint32_t addr)
 {
-    qemu_log("This was a read 0x%x addr:0x%x value:0x%x\n", env->active_tc.PC, addr, val);
+    // qemu_log("This was a read 0x%x addr:0x%x value:0x%x\n", env->active_tc.PC, addr, val);
 
-    OperandInfo *oi = load_store_mem(addr, val, 0, 4);
+    OperandInfo *oi = load_store_mem(addr, 0, val, 4);
 
     qemu_trace_add_operand(oi, 0x1);
 }
 
-void HELPER(trace_st)(CPUMIPSState *env, uint32_t val, uint32_t addr)
+void HELPER(trace_st)(CPUSH4State *env, uint32_t val, uint32_t addr)
 {
-    qemu_log("This was a store 0x%x addr:0x%x value:0x%x\n", env->active_tc.PC, addr, val);
+    // qemu_log("This was a store 0x%x addr:0x%x value:0x%x\n", env->active_tc.PC, addr, val);
 
-    OperandInfo *oi = load_store_mem(addr, val, 1, 4);
+    OperandInfo *oi = load_store_mem(addr, 1, val, 4);
 
     qemu_trace_add_operand(oi, 0x2);
 }
-*/
